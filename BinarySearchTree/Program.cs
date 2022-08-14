e2aeb21416ccd62a23b6c45ff04c13b0b0e3a500
Kundan <kundan kamble>
2022-08-14 02:41:18 -0800

    Create Bainary Tree

---
+++ BinarySearchTree/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace BinaryTree
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Binary Search Tree");
+            BinarySearchTree<int> binarySearchTree = new BinarySearchTree<int>(56);
+            binarySearchTree.AddNode(30);
+            binarySearchTree.AddNode(70);
+            binarySearchTree.AddNode(10);
+
+            binarySearchTree.Display();
+
+        }
+    }
+}
